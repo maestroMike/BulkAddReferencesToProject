d58348e269af99fa1f227751f89c3ba34acbe6a6
Mike Bardynin <mikebardynin@gmail.com>
2014-01-26 23:08:44 +0400

    add: projects

---
+++ dev.Common.Types/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev.Common.Types
+{
+    public class Class1
+    {
+    }
+}
